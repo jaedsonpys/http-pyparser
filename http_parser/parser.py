a353b5e910849650aebc40f653086849be57ce24
Jaedson Silva <contacanal798@gmail.com>
2022-07-06 16:18:54 -0300

    removing parameters from HTTPData.__init__

--- http_parser/parser.py
+++ http_parser/parser.py
@@ -1,19 +1,11 @@
 class HTTPData(object):
-    def __init__(
-        self,
-        path: str,
-        status: str,
-        body: str = None,
-        version: str = None,
-        headers: dict = None,
-        cookies: dict = None,
-    ) -> None:
-        self.path = path
-        self.status = status
-        self.version = version
-        self.body = body
-        self.headers = headers
-        self.cookies = cookies
+    def __init__(self) -> None:
+        self.path = None
+        self.status = None
+        self.version = None
+        self.body = None
+        self.headers = None
+        self.cookies = None
 
 
 class HTTPParser(object):
