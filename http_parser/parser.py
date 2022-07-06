8e4cf17570c2530b9962e437bbb29a7c7364f5ee
Jaedson Silva <contacanal798@gmail.com>
2022-07-06 20:26:42 -0300

    creating HTTPParser.parser method

--- http_parser/parser.py
+++ http_parser/parser.py
@@ -1,3 +1,6 @@
+from . import exceptions
+
+
 class HTTPData(object):
     def __init__(self) -> None:
         self.path = None
@@ -11,3 +14,33 @@ class HTTPData(object):
 class HTTPParser(object):
     def __init__(self) -> None:
         self.result = HTTPData()
+
+    def parser(self, http_message: str) -> HTTPData:
+        """Parser a HTTP request message.
+
+        All headers will be analyzed and added
+        to an instance of the `HTTPData` class,
+        which will be returned when everything
+        is finished.
+
+        :param http_message: HTTP request message;
+        :type http_message: str
+        :raises exceptions.InvalidHTTPMessageError:
+        Raises exception if message is invalid
+        :return: HTTP message data
+        :rtype: HTTPData
+        """
+
+        msg_parts = http_message.split('\r\n')
+
+        try:
+            info = msg_parts.pop(0)
+            method, path, version = info.split(' ')
+        except (ValueError, IndexError):
+            raise exceptions.InvalidHTTPMessageError('Invalid HTTP message')
+
+        self.result.method = method
+        self.result.path = path
+        self.result.version = version
+
+        return self.result
