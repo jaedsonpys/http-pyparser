6dbce68d9708842fd44ae16cf0585dd999b8b68b
Jaedson Silva <contacanal798@gmail.com>
2022-07-07 18:22:36 -0300

    creating unit test script in tests/

---
+++ tests/test_parser.py
@@ -0,0 +1,19 @@
+# This test uses BuPyTest (https://github.com/jaedsonpys/bupytest).
+# Follow the documentation to see how to run.
+
+import sys
+
+sys.path.insert('./')
+
+import bupytest
+import http_pyparser
+
+
+class TestParser(bupytest.UnitTest):
+    def __init__(self):
+        super().__init__()
+        self.parser = http_pyparser.HTTPParser()
+
+
+if __name__ == '__main__':
+    bupytest.this()
