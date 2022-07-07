73a4cb94393c451e6de27a36f691dd23799c1b4e
Jaedson Silva <contacanal798@gmail.com>
2022-07-07 18:14:29 -0300

    creating setup.py script

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from http_parser import __version__
+
+with open('README.md', 'r') as reader:
+    long_description = reader.read()
+
+setup(
+    author='Jaedson Silva',
+    author_email='imunknowuser@protonmail.com',
+    name='http-pyparser',
+    description='Parse HTTP messages simply and quickly with Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version=__version__,
+    packages=['http-parser'],
+    keywords=['http', 'parser', 'python', 'request'],
+    license='Apache 2.0',
+    platforms=['any'],
+    url='https://github.com/x07ex/http_parser',
+    python_requires='>= 3.6',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 Only',
+        'Topic :: Internet',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
