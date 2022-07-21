349982c85a9f5f4bb3b6d06dbb6ce6298eaaba8e
Jaedson Silva <contacanal798@gmail.com>
2022-07-21 20:56:00 -0300

    renaming "status" attribute to "method" in HTTPData

--- http_pyparser/parser.py
+++ http_pyparser/parser.py
@@ -5,7 +5,7 @@ from typing import Union
 class HTTPData(object):
     def __init__(self) -> None:
         self.path = None
-        self.status = None
+        self.method = None
         self.version = None
         
         self.host = None
@@ -18,7 +18,7 @@ class HTTPData(object):
         self.query = None
 
     def __repr__(self) -> str:
-        return (f'HTTPData(path={self.path}, status={self.status}, version={self.version}, '
+        return (f'HTTPData(path={self.path}, method={self.method}, version={self.version}, '
                 f'host={self.host}, user_agent={self.user_agent}, accept={self.accept}, '
                 f'body={self.body}, headers={self.headers}, cookies={self.cookies}, '
                 f'query={self.query})')
