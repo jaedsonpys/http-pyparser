2b127a2bfa1115da66b3385c942e47ad5d20d063
Jaedson Silva <contacanal798@gmail.com>
2022-07-21 20:57:18 -0300

    adding assert test to "method" and HTTP "version"

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -28,9 +28,12 @@ class TestParser(bupytest.UnitTest):
     def test_parse_message(self):
         parsed_http = self.parser.parser(HTTP_MESSAGE)
         
+        self.assert_expected(parsed_http.path, '/api/user')
+        self.assert_expected(parsed_http.method, 'GET')
+        self.assert_expected(parsed_http.version, 'HTTP/1.1')
+
         self.assert_expected(parsed_http.cookies, {'test_cookie': '0123456789'})
         self.assert_expected(parsed_http.query, {'email': 'test@gmail.com', 'age': '18'})
-        self.assert_expected(parsed_http.path, '/api/user')
 
 
 if __name__ == '__main__':
